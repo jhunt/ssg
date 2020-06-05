48bc9381f3bf2b5db14c4f1881b6b702902632d8
James Hunt <james@niftylogic.com>
2020-06-05 15:21:00 -0400

    Remove FIXME note (outdated)
    We are handling S3 credentials, so there's no "eventually" about it.

--- cmd/ssg/main.go
+++ cmd/ssg/main.go
@@ -52,8 +52,6 @@ func main() {
 
 		ControlUsername string `cli:"--control-username" env:"SSG_CONTROL_USERNAME"`
 		ControlPassword string `cli:"--control-password" env:"SSG_CONTROL_PASSWORD"`
-
-		// eventually: S3 creds
 	}
 
 	opts.Log = "info"
