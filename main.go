a1d57958acfa47ab621a8bba77d1f0dd99cc021a
James Hunt <james@niftylogic.com>
2020-06-08 16:56:33 -0400

    Add `ssg (upload|download|g=expunge|get|put)` commands

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"io"
 	"os"
 
 	fmt "github.com/jhunt/go-ansi"
@@ -9,6 +11,7 @@ import (
 	env "github.com/jhunt/go-envirotron"
 	"github.com/jhunt/go-log"
 
+	"github.com/jhunt/shield-storage-gateway/pkg/client"
 	"github.com/jhunt/shield-storage-gateway/pkg/ssg"
 )
 
@@ -19,32 +22,47 @@ func main() {
 		Help    bool `cli:"-h, --help"`
 		Version bool `cli:"-v, --version"`
 
-		Debug bool   `cli:"-D, --debug"     env:"SSG_DEBUG"`
-		Log   string `cli:"-L, --log-level" env:"SSG_LOG_LEVEL"`
+		Server struct {
+			Debug bool   `cli:"-D, --debug"     env:"SSG_DEBUG"`
+			Log   string `cli:"-L, --log-level" env:"SSG_LOG_LEVEL"`
 
-		Config string `cli:"-c, --config" env:"SSG_CONFIG"`
+			Config string `cli:"-c, --config" env:"SSG_CONFIG"`
+		} `cli:"server"`
+
+		Upload struct {
+			URL   string `cli:"-u, --url"   env:"SSG_URL"`
+			Token string `cli:"-t, --token" env:"SSG_CONTROL_TOKEN"`
+		} `cli:"upload"`
+
+		Download struct {
+			URL   string `cli:"-u, --url"   env:"SSG_URL"`
+			Token string `cli:"-t, --token" env:"SSG_CONTROL_TOKEN"`
+		} `cli:"download"`
+
+		Expunge struct {
+			URL   string `cli:"-u, --url"   env:"SSG_URL"`
+			Token string `cli:"-t, --token" env:"SSG_STREAM_TOKEN"`
+		} `cli:"expunge, delete, rm"`
+
+		Get struct {
+			URL   string `cli:"-u, --url"   env:"SSG_URL"`
+			Token string `cli:"-t, --token" env:"SSG_STREAM_TOKEN"`
+		} `cli:"get"`
+
+		Put struct {
+			URL   string `cli:"-u, --url"   env:"SSG_URL"`
+			Token string `cli:"-t, --token" env:"SSG_STREAM_TOKEN"`
+		} `cli:"put"`
 	}
 
-	opts.Log = "info"
-	opts.Config = "/etc/ssg/ssg.yml"
+	opts.Server.Log = "info"
+	opts.Server.Config = "/etc/ssg/ssg.yml"
 	env.Override(&opts)
-	_, args, err := cli.Parse(&opts)
+	command, args, err := cli.Parse(&opts)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "!!! %s\n", err)
 		os.Exit(1)
 	}
-	if len(args) != 0 {
-		fmt.Fprintf(os.Stderr, "!!! extra arguments found\n")
-		os.Exit(1)
-	}
-
-	if opts.Debug {
-		opts.Log = "debug"
-	}
-	log.SetupLogging(log.LogConfig{
-		Type:  "console",
-		Level: opts.Log,
-	})
 
 	if opts.Help {
 		fmt.Printf("ssg - The SHIELD Storage Gateway\n\n")
@@ -76,15 +94,195 @@ func main() {
 		os.Exit(0)
 	}
 
-	s, err := ssg.NewServerFromFile(opts.Config)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "!! @R{%s}\n", err)
-		os.Exit(1)
-	}
-	if err := s.Run(vers); err != nil {
-		fmt.Fprintf(os.Stderr, "!! @R{%s}\n", err)
-		os.Exit(2)
+	if command == "server" {
+		if len(args) != 0 {
+			fmt.Fprintf(os.Stderr, "!!! extra arguments found\n")
+			os.Exit(1)
+		}
+		if opts.Server.Debug {
+			opts.Server.Log = "debug"
+		}
+		log.SetupLogging(log.LogConfig{
+			Type:  "console",
+			Level: opts.Server.Log,
+		})
+
+		s, err := ssg.NewServerFromFile(opts.Server.Config)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "!! @R{%s}\n", err)
+			os.Exit(1)
+		}
+		if err := s.Run(vers); err != nil {
+			fmt.Fprintf(os.Stderr, "!! @R{%s}\n", err)
+			os.Exit(2)
+		}
+
+		os.Exit(0)
 	}
 
-	os.Exit(0)
+	if command == "upload" {
+		if opts.Upload.Token == "" {
+			fmt.Fprintf(os.Stderr, "!! missing required @Y{--token}\n")
+			os.Exit(2)
+		}
+		if opts.Upload.URL == "" {
+			fmt.Fprintf(os.Stderr, "!! missing required @Y{--url}\n")
+			os.Exit(1)
+		}
+		if len(args) < 1 {
+			fmt.Fprintf(os.Stderr, "!! missing required REMOTE-PATH argument\n")
+			os.Exit(1)
+		}
+		if len(args) > 1 {
+			fmt.Fprintf(os.Stderr, "!! extra arguments found\n")
+			os.Exit(1)
+		}
+
+		c := client.Controller{
+			URL:   opts.Upload.URL,
+			Token: opts.Upload.Token,
+		}
+
+		stream, err := c.NewUpload(args[0])
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "!! @W{/control} failed: @R{%s}\n", err)
+			os.Exit(2)
+		}
+
+		b, err := json.MarshalIndent(stream, "", "  ")
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "!! failed to json: @R{%s}\n", err)
+			os.Exit(3)
+		}
+		fmt.Printf("%s\n", string(b))
+		os.Exit(0)
+	}
+
+	if command == "download" {
+		if opts.Download.Token == "" {
+			fmt.Fprintf(os.Stderr, "!! missing required @Y{--token}\n")
+			os.Exit(1)
+		}
+		if opts.Download.URL == "" {
+			fmt.Fprintf(os.Stderr, "!! missing required @Y{--url}\n")
+			os.Exit(1)
+		}
+		if len(args) < 1 {
+			fmt.Fprintf(os.Stderr, "!! missing required REMOTE-PATH argument\n")
+			os.Exit(1)
+		}
+		if len(args) > 1 {
+			fmt.Fprintf(os.Stderr, "!! extra arguments found\n")
+			os.Exit(1)
+		}
+
+		c := client.Controller{
+			URL:   opts.Download.URL,
+			Token: opts.Download.Token,
+		}
+
+		stream, err := c.NewDownload(args[0])
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "!! @W{/control} failed: @R{%s}\n", err)
+			os.Exit(2)
+		}
+
+		b, err := json.MarshalIndent(stream, "", "  ")
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "!! failed to json: @R{%s}\n", err)
+			os.Exit(3)
+		}
+		fmt.Printf("%s\n", string(b))
+		os.Exit(0)
+	}
+
+	if command == "expunge" {
+		if opts.Expunge.Token == "" {
+			fmt.Fprintf(os.Stderr, "!! missing required @Y{--token}\n")
+			os.Exit(1)
+		}
+		if opts.Expunge.URL == "" {
+			fmt.Fprintf(os.Stderr, "!! missing required @Y{--url}\n")
+			os.Exit(1)
+		}
+		if len(args) < 1 {
+			fmt.Fprintf(os.Stderr, "!! missing required REMOTE-PATH argument\n")
+			os.Exit(1)
+		}
+		if len(args) > 1 {
+			fmt.Fprintf(os.Stderr, "!! extra arguments found\n")
+			os.Exit(1)
+		}
+
+		c := client.Controller{
+			URL:   opts.Expunge.URL,
+			Token: opts.Expunge.Token,
+		}
+
+		err := c.Expunge(args[0])
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "!! @W{/control} failed: @R{%s}\n", err)
+			os.Exit(2)
+		}
+		os.Exit(0)
+	}
+
+	if command == "get" {
+		if opts.Get.Token == "" {
+			fmt.Fprintf(os.Stderr, "!! missing required @Y{--token}\n")
+			os.Exit(1)
+		}
+		if opts.Get.URL == "" {
+			fmt.Fprintf(os.Stderr, "!! missing required @Y{--url}\n")
+			os.Exit(1)
+		}
+		if len(args) < 1 {
+			fmt.Fprintf(os.Stderr, "!! missing required STREAM-ID argument\n")
+			os.Exit(1)
+		}
+		if len(args) > 1 {
+			fmt.Fprintf(os.Stderr, "!! extra arguments found\n")
+			os.Exit(1)
+		}
+
+		c := client.Customer{
+			URL: opts.Get.URL,
+		}
+		rd, err := c.Download(args[0], opts.Get.Token)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "!! @R{%s}\n", err)
+		}
+		io.Copy(os.Stdout, rd)
+		rd.Close()
+		os.Exit(0)
+	}
+
+	if command == "put" {
+		if opts.Put.Token == "" {
+			fmt.Fprintf(os.Stderr, "!! missing required @Y{--token}\n")
+			os.Exit(1)
+		}
+		if opts.Put.URL == "" {
+			fmt.Fprintf(os.Stderr, "!! missing required @Y{--url}\n")
+			os.Exit(1)
+		}
+		if len(args) < 1 {
+			fmt.Fprintf(os.Stderr, "!! missing required STREAM-PATH argument\n")
+			os.Exit(1)
+		}
+		if len(args) > 1 {
+			fmt.Fprintf(os.Stderr, "!! extra arguments found\n")
+			os.Exit(1)
+		}
+
+		c := client.Customer{
+			URL: opts.Get.URL,
+		}
+		n, err := c.Upload(args[0], opts.Put.Token, os.Stdin, true)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "!! @R{%s}\n", err)
+		}
+		fmt.Fprintf(os.Stderr, "uploaded %d bytes\n", n)
+		os.Exit(0)
+	}
 }
---
+++ pkg/client/controller.go
@@ -0,0 +1,76 @@
+package client
+
+import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"strings"
+)
+
+type Controller struct {
+	URL   string
+	Token string
+
+	Client *http.Client
+}
+
+func (cc *Controller) url(rest ...string) string {
+	base := strings.TrimSuffix(cc.URL, "/")
+	return base + "/" + strings.Join(rest, "/")
+}
+
+func (cc *Controller) control(kind, target string) (*Stream, error) {
+	if cc.Client == nil {
+		cc.Client = &http.Client{}
+	}
+
+	b, err := json.Marshal(struct {
+		Kind   string `json:"kind"`
+		Target string `json:"target"`
+	}{
+		Kind:   kind,
+		Target: target,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest("POST", cc.url("control"), bytes.NewBuffer(b))
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Accept", "application/json")
+	req.Header.Set("Authorization", "Bearer "+cc.Token)
+
+	res, err := cc.Client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	b, err = ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var out Stream
+	if err := json.Unmarshal(b, &out); err != nil {
+		return nil, err
+	}
+	return &out, nil
+}
+
+func (cc *Controller) NewUpload(target string) (*Stream, error) {
+	return cc.control("upload", target)
+}
+
+func (cc *Controller) NewDownload(target string) (*Stream, error) {
+	return cc.control("download", target)
+}
+
+func (cc *Controller) Expunge(target string) error {
+	_, err := cc.control("expunge", target)
+	return err
+}
